30b2644ea5f3a2d14ab4e98de3a66ba61dd32c57
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-06-06 16:42:11 -0300

    Database configuration

---
+++ EF/WebCrawlerContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    public class WebCrawlerContext:DbContext
+    {
+        private string ConnectionString { get; set; }
+
+        public WebCrawlerContext()
+        {
+            
+        }
+
+        public WebCrawlerContext(IConfiguration configuration)
+        {
+            ConnectionString = configuration["ConnString"];
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(this.ConnectionString,ServerVersion.AutoDetect(this.ConnectionString));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+using WebCrawler;
+
+HttpClient client = new HttpClient();
+var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
+IConfiguration configuration = builder.Build();
+WebCrawlerContext context = new WebCrawlerContext(configuration);
+
+Console.WriteLine(configuration["ConnString"]);
